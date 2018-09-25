0289865ee609b937d413cf85b64f67fdef7cfec2
C790266922 <790266922@qq.com>
2018-09-26 07:32:40 +0800

    update

---
+++ okex/run.sh
@@ -0,0 +1,2 @@
+nohup python3 runner.py strategy/future/ma_beta.py strategy/future/config/ma_beta.json > log/future.log &
+nohup python3 runner.py strategy/spot/ma.py strategy/spot/config/ma.json > log/spot.log &
